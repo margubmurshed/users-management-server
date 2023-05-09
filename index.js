e916670d88533592b511caf89fcc79a749129412
Margub Murshed <murshedmargub@gmail.com>
2023-05-09 21:23:50 +0600

    users get and post api done

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const users = require('./data/users.json');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("Users management server started"));
+app.get("/users", (req,res) => res.send(users))
+app.post("/users", (req, res) => {
+    users.push(req.body);
+    res.send(users)
+})
 
 app.listen(port)
